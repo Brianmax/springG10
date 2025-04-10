cd8d11f5be72f1550b01b1bf7cdbbadd28c5863c
George Maxi <georgemaxi@Georges-Mac-mini.local>
2025-04-09 19:10:50 -0500

    query params

--- src/main/java/com/codigo/spring/controller/AvionController.java
+++ src/main/java/com/codigo/spring/controller/AvionController.java
@@ -4,6 +4,8 @@ import com.codigo.spring.entity.AvionEntity;
 import com.codigo.spring.repository.AvionRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/avion")
 public class AvionController {
@@ -15,6 +17,18 @@ public class AvionController {
     @PostMapping("/save")
     public AvionEntity save(@RequestBody AvionEntity avionEntity) {
         return avionRepository.save(avionEntity);
+    }
+
+    @GetMapping("/find/modelo")
+    public List<AvionEntity> findAll(@RequestParam String modelo) {
+        List<AvionEntity> avionEntities1 = avionRepository.findByModelo(modelo);
+        List<AvionEntity> avionEntities2 = avionRepository.customFindByModelo(modelo);
+        return avionEntities1;
+    }
+
+    @GetMapping("/find/capacidad")
+    public List<AvionEntity> findAllCapacidad(@RequestParam int min, @RequestParam int max) {
+        return avionRepository.findByCapacidad(min, max);
     }
 
 }
