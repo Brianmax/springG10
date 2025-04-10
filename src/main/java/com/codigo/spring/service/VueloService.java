81358883fd7f05eb374ebf79074427277e0cf6a9
George Maxi <georgemaxi@Georges-Mac-mini.local>
2025-04-09 19:57:18 -0500

    vuelo

---
+++ src/main/java/com/codigo/spring/service/VueloService.java
@@ -0,0 +1,9 @@
+package com.codigo.spring.service;
+
+import com.codigo.spring.entity.VueloEntity;
+import com.codigo.spring.request.VueloRequest;
+
+public interface VueloService {
+    VueloEntity save(VueloRequest vueloRequest);
+    VueloEntity findById(Long id);
+}
