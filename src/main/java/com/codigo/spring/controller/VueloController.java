be5ea66d323e5798953e85e8103a3326c67532ab
George Maxi <georgemaxi@Georges-Mac-mini.local>
2025-04-14 20:34:49 -0500

    query pilotos by vuelo

--- src/main/java/com/codigo/spring/controller/VueloController.java
+++ src/main/java/com/codigo/spring/controller/VueloController.java
@@ -2,6 +2,7 @@ package com.codigo.spring.controller;
 
 import com.codigo.spring.entity.VueloEntity;
 import com.codigo.spring.request.VueloRequest;
+import com.codigo.spring.request.VueloRequestUpdatePilotos;
 import com.codigo.spring.response.VueloResponse;
 import com.codigo.spring.service.VueloService;
 import org.springframework.web.bind.annotation.*;
@@ -33,4 +34,11 @@ public class VueloController {
     public List<VueloResponse> findByFecha(@RequestParam Date fechaSalida) {
         return vueloService.findAllByFechaSalida(fechaSalida);
     }
+    // actualizar el avion de un determinado vuelo
+
+    @PutMapping("/update/pilotos")
+    public VueloResponse updatePilotos(@RequestBody VueloRequestUpdatePilotos vueloRequest) {
+        return vueloService.addPiltosToVuelo(vueloRequest);
+    }
+
 }
